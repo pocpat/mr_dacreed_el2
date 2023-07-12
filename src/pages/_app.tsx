656adb1e22e36759684629ae3d94a9499006420b
JonoPadoa <jonathanp@missionreadyhq.com>
2023-07-13 11:22:01 +1200

    Clerk Auth and SupaBase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +12,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
+      </ClerkProvider>
     </SessionProvider>
   );
 };
