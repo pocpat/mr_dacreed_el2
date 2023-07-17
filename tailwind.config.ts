595831865c978669793c0e97676acf3e1e8e1f80
JonoPadoa <jonathanp@missionreadyhq.com>
2023-07-18 11:42:10 +1200

    DB work - SupaBase Schema

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
 import { type Config } from "tailwindcss";
+import typography from '@tailwindcss/typography';
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require ("@tailwindcss/typography"), require("daisyui")],
 } satisfies Config;
+
+
