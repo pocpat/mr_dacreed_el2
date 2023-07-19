5976674b2c90ba117eaa6cb659bbe353e0f74da8
Elena Kroupkin <pocoat@gmail.com>
2023-07-20 11:21:55 +1200

    change Admin href

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg- bg-[#7ebfb3] p-4 text-white hover:bg-white/50 drop-shadow-md"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="admin/admin"
             >
               <h3 className="text-2xl font-bold">ADMIN</h3>
             </Link>
