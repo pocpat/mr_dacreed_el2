a038dfe29938da3379fcb95915ea97bbdb7ea0de
Elena Kroupkin <pocoat@gmail.com>
2023-07-20 15:24:38 +1200

    cleanup index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
-import { SignIn, SignUp, useUser, SignInButton,  SignOutButton } from "@clerk/nextjs";
-import {Header} from "../componentsRoot/Header";
+import { useUser, SignInButton, SignOutButton } from "@clerk/nextjs";
+import { Header } from "../componentsRoot/Header";
 import { type NextPage } from "next";
 
-
-  const Home : NextPage = () => {
-  // const hello = api.example.hello.useQuery({ 
-  //   text: "World", 
-  // });
-
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//   undefined, // no input
-//   { enabled: sessionData?.user !== undefined }
-// );
+const Home: NextPage = () => {
   const user = useUser();
   return (
     <>
@@ -23,7 +14,7 @@ import { type NextPage } from "next";
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#4f7369] to-[#A7F2E4]">
-       <Header/>
+        <Header />
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white drop-shadow-md sm:text-[5rem]">
             {" "}
@@ -44,7 +35,6 @@ import { type NextPage } from "next";
             </Link>
             <Link
               className="bg- flex max-w-xs flex-col gap-4 rounded-xl bg-[#7ebfb3] p-4 text-white drop-shadow-md hover:bg-white/50"
-              // href="https://create.t3.gg/en/introduction"
               href="/namedLibrary/namedLibrary"
             >
               <h3 className="text-2xl font-bold">LIBRARY</h3>
@@ -52,12 +42,10 @@ import { type NextPage } from "next";
           </div>
           <div className="flex flex-col items-center gap-2">
             <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20">
-              {!user.isSignedIn && <SignInButton mode="modal"/>}
+              {!user.isSignedIn && <SignInButton mode="modal" />}
               {user.isSignedIn && <SignOutButton />}
-         </div>
-            <div> 
             </div>
-
+            <div></div>
           </div>
         </div>
       </main>
@@ -83,4 +71,4 @@ const AuthShowcase: React.FC = () => {
       <SignInButton />
     </div>
   );
-}
+};
