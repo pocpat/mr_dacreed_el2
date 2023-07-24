73ea07dfe49d378b00732e3e934b0c8896377200
Elena Kroupkin <pocoat@gmail.com>
2023-07-24 19:37:30 +1200

    changed greeting in the Header

--- src/componentsRoot/Header.tsx
+++ src/componentsRoot/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <div className="navbar bg-primary text-primary-content">  
       <div className="flex-1 pl-5 text-3xl font-bold">
-        {user?.firstName ? `Notes for ${user.firstName}` : ""}
+        {user?.firstName ? `Hello ${user.firstName}` : ""}
       </div>
         <div className="flex-none gap-2">
 <div className="dropdown dropdown-end">
