d408537225433a951e12c0fad9272f283bd59662
Elena Kroupkin <pocoat@gmail.com>
2023-07-24 22:41:41 +1200

    prepare 3 placeholders in Admin

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -1,21 +1,35 @@
 // import { Content } from 'next/font/google'
 import React from 'react'
 import { Header } from '~/componentsRoot/Header'
+import TextAreaForAdmin from '../components/elenasComponents/TextAreaForAdmin'
 const admin = () => {
   return (
     <>
 
 <Header />
-<div><h1>CREATE    REVIEW    APPROVE</h1></div>
+<div className='
+// flex flex-row items-center justify-center 
+grid grid-flow-row-dense grid-cols-3 grid-rows-3
+
+'><h1>CREATE    REVIEW    APPROVE</h1></div>
 
 
-<div className="flex flex-row items-center justify-center gap-12 px-4 py-16 ">
 
-<section className="flex flex-col items-center justify-center gap-12 px-4 py-16 "></section>
-<section className="flex flex-col items-center justify-center gap-12 px-4 py-16 "></section>
-<section className="flex flex-col items-center justify-center gap-12 px-4 py-16 "></section>
-</div>
     
+    <div className="p-8 bg-slate-800">
+  <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8">
+    <section className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">Left</section>
+    <section className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">Middle</section>
+    <section className="p-4 bg-cyan-400 rounded-md flex items-center justify-center">Right</section>
+  </div>
+</div>
+
+
+
+
+
+
+
     </>
   )
 }
---
+++ src/pages/components/elenasComponents/TextAreaForAdmin.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TextAreaFrorAdmin = () => {
+  return (
+    <textarea
+    placeholder="Bio"
+    className="textarea-bordered textarea textarea-lg w-full max-w-xs"
+  ></textarea>
+  )
+}
+
+export default TextAreaFrorAdmin
