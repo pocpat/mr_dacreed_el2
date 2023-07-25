c1bb24c5c48ec36d6816ec5517adc4462fb81882
Elena Kroupkin <pocoat@gmail.com>
2023-07-26 10:58:23 +1200

    basic function buttions from two sides

--- src/pages/components/elenasComponents/TextEditor.tsx
+++ src/pages/components/elenasComponents/TextEditor.tsx
@@ -1,54 +1,57 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import "react-quill/dist/quill.snow.css";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
+interface TextEditorProps {
+  content: string;
+}
 
-const ReactQuill = dynamic(
-  () => import('react-quill'),
-  { ssr: false }
-);
-
-const TextEditor = ({ content }) => {
-  const [value, setValue] = useState ('');
-
+const TextEditor: React.FC<TextEditorProps> = ({ content }) => {
+  const [value, setValue] = useState("");
 
   useEffect(() => {
     setValue(content);
   }, [content]);
 
   const toolbarOptions = [
-    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-  ['blockquote', 'code-block'],
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
 
-  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-  [{ 'direction': 'rtl' }],                         // text direction
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
 
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
-  [{ 'align': [] }],
-  ['link', 'image'],
-  ['clean']              
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+    ["link", "image"],
+    ["clean"],
   ];
   const editorModule = {
     toolbar: toolbarOptions,
   };
   return (
     <div>
-      <h2 className='bg-primary-content font-bold text-neutral-500'>text edditor</h2>
-      <ReactQuill 
-      modules = {editorModule}
-      theme = "snow"
-      value={value}
-      />
+      <h2 className="bg-primary-content font-bold text-neutral-500">
+        text edditor
+      </h2>
+      <ReactQuill modules={editorModule} theme="snow" value={value} />
+      <style jsx>{`
+        /* Apply styles to the code blocks in the Quill editor */
+        .ql-snow .ql-editor pre.ql-syntax {
+          @apply bg-blue-900 text-white; // Tailwind utility classes for background color and text color
+          overflow: visible;
+        }
+      `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default TextEditor
+export default TextEditor;
--- src/pages/input/waiting/editing/elena1.tsx
+++ src/pages/input/waiting/editing/elena1.tsx
@@ -3,7 +3,7 @@ import { Header } from '~/componentsRoot/Header'
 import TextAreaForAdmin from '../../../components/elenasComponents/TextAreaForAdmin'
 import ButtonGroup from '../../../components/elenasComponents/ButtonGroup'
 import TextEditor from '~/pages/components/elenasComponents/TextEditor'
-
+import ButtonsQ from '../../../components/elenasComponents/ButtonsQ'
 const Admin = () => {
     const [selectedButtonContent, setSelectedButtonContent] = useState('');
 
@@ -18,12 +18,15 @@ flex flex-row items-center justify-center '>
     <h1>CREATE         REVIEW          APPROVE</h1></div>
     <div className="p-8 bg-slate-800">
   <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8">
-    <section className="p-4 px-1 bg-cyan-400 rounded-md flex items-center justify-center col-span-1">Left</section>
+    <section className="p-4 px-1 bg-cyan-400 rounded-md flex items-center justify-center col-span-1">
+    <ButtonsQ onSelect={handleButtonSelection}/> 
+    
+    </section>
     <section className="p-4 bg-cyan-400 rounded-md flex items-center justify-center grid-cols-1 flex-col col-span-2">
     <TextEditor content={selectedButtonContent}/>
     <TextAreaForAdmin />
     </section>
-    <section className="p-4 px-1 bg-cyan-400 rounded-md flex items-center justify-center col-span-1">Right
+    <section className="p-4 px-1 bg-cyan-400 rounded-md flex items-center justify-center col-span-1">
     <ButtonGroup onSelect={handleButtonSelection} />
     </section>
   </div>
