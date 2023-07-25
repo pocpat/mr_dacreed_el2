0ee364eeac53d2581954df0f23316b3554aff3d0
JonoPadoa <jonathanp@missionreadyhq.com>
2023-07-25 16:43:33 +1200

    Merged jp_working into jp_development for deployment test

--- src/pages/components/jpComponents/TopNav.tsx
+++ src/pages/components/jpComponents/TopNav.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const TopNav = () => {
   return (
@@ -10,31 +11,31 @@ const TopNav = () => {
               <div className="flex flex-shrink-0 items-center"></div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
-                  <a
+                  <Link
                     href="/"
                     className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                     aria-current="page"
                   >
                     Home
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="#"
                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                   >
                     Team
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="#"
                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                   >
                     Projects
-                  </a>
-                  <a
+                  </Link>
+                  <Link
                     href="#"
                     className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                   >
                     Calendar
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
