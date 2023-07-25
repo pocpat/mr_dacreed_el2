aa94c5f24c3a80d00e33f14f5f8ec49ec596be7c
JonoPadoa <jonathanp@missionreadyhq.com>
2023-07-25 16:18:35 +1200

    Page and components

---
+++ src/pages/components/jpComponents/MainView.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainView = () => {
+  return (
+    <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl lg:flex lg:px-24 lg:pt-0">
+      <div className="mx-auto max-w-md bg-gray-500 text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          JP TESTER SECTION
+        </h2>
+      </div>
+    </div>
+  );
+};
+export default MainView;
--- src/pages/namedLibrary/namePage/jonosPage.tsx
+++ src/pages/namedLibrary/namePage/jonosPage.tsx
@@ -2,18 +2,36 @@
 // import { signIn, signOut, useSession } from "next-auth/react";
 // import Head from "next/head";
 import Link from "next/link";
+import MainView from "~/pages/components/jpComponents/MainView";
 // import { api } from "~/utils/api";
 import TopNav from "~/pages/components/jpComponents/TopNav";
 
 export default function jonosPage() {
+  const styleContainer = {
+    width: "100vw",
+    height: "60vh",
+    backgroundColor: "#082f49",
+    // border: "1px solid black",
+    padding: "10px",
+  };
+  const styleSection1 = {
+    // width: "100vw",
+    // height: "vh",
+    // backgroundColor: "#082f49",
+    // // border: "1px solid black",
+    // padding: "10px",
+  };
+
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#09090b] to-[#d4d4d8]">
         <div>
-          <TopNav />
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span style={{ color: "white" }}>Jp Test Page</span>
-          </h1>
+          <div style={styleSection1}>
+            <TopNav />
+          </div>
+          <div style={styleContainer}>
+            <MainView />
+          </div>
         </div>
       </main>
     </div>
