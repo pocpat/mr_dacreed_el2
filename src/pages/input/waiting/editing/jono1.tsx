da372414cb87f7415a1cc237efaadf56d3d8d3ee
JonoPadoa <jonathanp@missionreadyhq.com>
2023-07-28 11:54:08 +1200

    added placeholder into Jono1 and Jono2 and also changed the path in namedLibrary from JonosPage to jonosPage as had a previous issue with the naming convention but it seems to be working now.

---
+++ src/pages/input/waiting/editing/jono1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const jono1 = () => {
+  return (
+    <div>
+      <h1>Placeholder for now.</h1>
+    </div>
+  );
+};
+
+export default jono1;
--- src/pages/namedLibrary/namedLibrary.tsx
+++ src/pages/namedLibrary/namedLibrary.tsx
@@ -25,7 +25,7 @@ export default function JPLibrary() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-[#7ebfb3] p-4 text-white hover:bg-white/20"
-              href="/namedLibrary/namePage/JonosPage"
+              href="/namedLibrary/namePage/jonosPage"
               // target="_blank"
             >
               <h3 className="text-2xl font-bold">Jonos Library</h3>
