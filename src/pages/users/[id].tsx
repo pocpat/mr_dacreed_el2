fc45f8d5005ef70f11d6ec468896e26280f695e0
Elena Kroupkin <pocoat@gmail.com>
2023-08-03 15:26:57 +1200

    [id].tsx fix export default

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,5 +1,7 @@
+// pages/users/[id].js (or .tsx if you are using TypeScript)
+
 import { useSession } from "next-auth/react";
-import  {SignIn}  from "~/componentsRoot/SignIn";
+import { SignIn } from "@clerk/nextjs";
 import { getServerAuthSession } from "~/server/auth";
 import { type GetServerSideProps } from "next";
 
@@ -9,6 +11,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     props: { session },
   };
 };
+
 const User = () => {
   const { data: session } = useSession();
 
@@ -19,3 +22,38 @@ const User = () => {
 
   return <p>Welcome {session.user.name}!</p>;
 };
+
+export default User;
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useSession } from "next-auth/react";
+// import  {SignIn}  from "@clerk/nextjs";
+// import { getServerAuthSession } from "~/server/auth";
+// import { type GetServerSideProps } from "next";
+
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const session = await getServerAuthSession(ctx);
+//   return {
+//     props: { session },
+//   };
+// };
+// const User = () => {
+//   const { data: session } = useSession();
+
+//   if (!session) {
+//     // Handle unauthenticated state, e.g. render a SignIn component
+//     return <SignIn />;
+//   }
+
+//   return <p>Welcome {session.user.name}!</p>;
+// };
