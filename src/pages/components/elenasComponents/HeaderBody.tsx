003250e0b9ad69ac3d5b9e70c6d1f2d51dee2f1a
Elena Kroupkin <pocoat@gmail.com>
2023-08-07 11:24:32 +1200

    accordion with Header Middle . in the Header - typing, chars calculations

--- src/pages/components/elenasComponents/HeaderBody.tsx
+++ src/pages/components/elenasComponents/HeaderBody.tsx
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from "react";
-import TextEditor from "./TextEditor";
-import Quill, { DeltaStatic } from "quill";
-import hljs from "highlight.js";
+import React, { useState } from "react";
 
 const HeaderBody = () => {
   const [charsLeft, setCharsLeft] = useState([150, 150, 260]);
   const [showTextEditor, setShowTextEditor] = useState(false);
   const [textAreaValue, setTextAreaValue] = useState(["", "", ""]);
 
-
-  const processTextAreaInput =( index: number, maxLength : number, event: React.ChangeEvent<HTMLTextAreaElement> ) => {
-    setTextAreaValue((textAreaValue)=> {
+  const processTextAreaInput = (
+    index: number,
+    maxLength: number,
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setTextAreaValue((textAreaValue) => {
       textAreaValue[index] = event.target.value;
       return textAreaValue;
     });
+
     const currentLength = event.target.value.length;
     setCharsLeft((charsLeft) => {
       const newCharsLeft = [...charsLeft];
       newCharsLeft[index] = maxLength - currentLength;
       return newCharsLeft;
     });
-
-  }
-
-
-  const handleIconClick = () => {
-    setShowTextEditor(true);
   };
+
+  // const handleIconClick = () => {
+  //   setShowTextEditor(true);
+  // };
 
   return (
     <>
@@ -34,7 +33,7 @@ const HeaderBody = () => {
         className="border-box flex flex-col items-center justify-start bg-gray-400 p-4 "
         style={{ width: "100%", height: "100vh" }}
       >
-        {/* Title */}
+ {/* Title */}
         <div className="form-control w-full">
           <label className="label">
             <span className="label-text font-semibold">Title</span>
@@ -46,12 +45,72 @@ const HeaderBody = () => {
             onChange={(e) => processTextAreaInput(0, 150, e)}
             maxLength={150}
             placeholder="Title here"
-            className="textarea-bordered textarea textarea-xs w-full rounded-sm shadow-md"
+            className="shadow-inner-md textarea-bordered textarea textarea-xs w-full rounded-sm"
             style={{ width: "100%" }}
+            // z-index='-1'
             // readOnly= {false}
           ></textarea>
-          {/* editing icon */}
-         </div>
+
+          {/* labels */}
+          <label className="label">
+            <span className="label-text-alt">
+              Characters left: {charsLeft[0]}
+            </span>
+            <span className="label-text-alt">Re-generate</span>
+          </label>
+        </div>
+
+ {/* Sub-heading */}
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text font-semibold">Sub-heading</span>
+            {/* <span className="label-text-alt">Alt label</span> */}
+          </label>
+
+          <textarea
+            value={textAreaValue[1]}
+            onChange={(e) => processTextAreaInput(1, 150, e)}
+            maxLength={150}
+            placeholder="Sub-heading here"
+            className="textarea-bordered textarea textarea-xs w-full rounded-sm shadow-md"
+            style={{ width: "100%" }}
+
+          ></textarea>
+          {/* labels */}
+          <label className="label">
+            <span className="label-text-alt">
+              Characters left: {charsLeft[1]}
+            </span>
+            <span className="label-text-alt">Re-generate</span>
+          </label>
+        </div>
+
+{/* Description */}
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text font-semibold">Description</span>
+            {/* <span className="label-text-alt">Alt label</span> */}
+          </label>
+
+          <textarea
+            value={textAreaValue[2]}
+            onChange={(e) => processTextAreaInput(2, 150, e)}
+            maxLength={150}
+            placeholder="Description here"
+            className="textarea-bordered textarea  h-48 w-full rounded-sm shadow-md"
+            style={{ width: "100%" }}
+          ></textarea>
+
+          {/* labels */}
+          <label className="label">
+            <span className="label-text-alt">
+              Characters left: {charsLeft[2]}
+            </span>
+            <span className="label-text-alt">Re-generate</span>
+          </label>
+        </div>
+
+        
       </div>
     </>
   );
