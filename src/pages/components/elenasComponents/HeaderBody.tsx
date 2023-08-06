460f890f85b14f19b48facfea93f63963f0726e4
Elena Kroupkin <pocoat@gmail.com>
2023-08-06 23:11:33 +1200

    elenaComon1 branch - get input Title working , prepare for the next 2 inputs

--- src/pages/components/elenasComponents/HeaderBody.tsx
+++ src/pages/components/elenasComponents/HeaderBody.tsx
@@ -6,26 +6,23 @@ import hljs from "highlight.js";
 const HeaderBody = () => {
   const [charsLeft, setCharsLeft] = useState([150, 150, 260]);
   const [showTextEditor, setShowTextEditor] = useState(false);
-  const [textAreaValue, setTextAreaValue] = useState("");
+  const [textAreaValue, setTextAreaValue] = useState(["", "", ""]);
 
-  useEffect(() => {
-    const textareas = document.querySelectorAll("textarea");
-    textareas.forEach((textarea, index) => {
-      if (textarea) {
-        const maxLength = parseInt(textarea.getAttribute("maxlength") || "0");
 
-        textarea.addEventListener("input", (event) => {
-          const target = event.currentTarget as HTMLTextAreaElement;
-          const currentLength = target.value.length;
-          setCharsLeft((charsLeft) => {
-            const newCharsLeft = [...charsLeft];
-            newCharsLeft[index] = maxLength - currentLength;
-            return newCharsLeft;
-          });
-        });
-      }
+  const processTextAreaInput =( index: number, maxLength : number, event: React.ChangeEvent<HTMLTextAreaElement> ) => {
+    setTextAreaValue((textAreaValue)=> {
+      textAreaValue[index] = event.target.value;
+      return textAreaValue;
     });
-  }, []);
+    const currentLength = event.target.value.length;
+    setCharsLeft((charsLeft) => {
+      const newCharsLeft = [...charsLeft];
+      newCharsLeft[index] = maxLength - currentLength;
+      return newCharsLeft;
+    });
+
+  }
+
 
   const handleIconClick = () => {
     setShowTextEditor(true);
@@ -45,78 +42,16 @@ const HeaderBody = () => {
           </label>
 
           <textarea
-            value={textAreaValue}
-            onChange={(e) => setTextAreaValue(e.target.value)}
+            value={textAreaValue[0]}
+            onChange={(e) => processTextAreaInput(0, 150, e)}
             maxLength={150}
             placeholder="Title here"
             className="textarea-bordered textarea textarea-xs w-full rounded-sm shadow-md"
             style={{ width: "100%" }}
+            // readOnly= {false}
           ></textarea>
           {/* editing icon */}
-          <button
-            className="absolute right-0 top-0 m-2"
-            onClick={handleIconClick}
-          >
-            📝
-          </button>
-          {showTextEditor && (
-            <div className="fixed left-0 top-0 z-10 h-full w-full bg-white p-4">
-              <TextEditor content={textAreaValue} />
-              <button onClick={() => setShowTextEditor(false)}>Close</button>
-            </div>
-          )}
-          <label className="label">
-            <span className="label-text-alt">
-              Characters left: {charsLeft[0]}{" "}
-            </span>
-            <span className="label-text-alt">Re-generate</span>
-          </label>
-          {/* Quill editor container */}
-          <div id="editor-container"></div>
-        </div>
-
-        {/* Sub-heading */}
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-semibold">Sub-heading</span>
-            {/* <span className="label-text-alt">Alt label</span> */}
-          </label>
-
-          <textarea
-            value={textAreaValue}
-            onChange={(e) => setTextAreaValue(e.target.value)}
-            maxLength={150}
-            placeholder="Sub-heading here"
-            className="textarea-bordered textarea textarea-xs w-full  rounded-sm shadow-md"
-          ></textarea>
-          <label className="label">
-            <span className="label-text-alt">
-              Characters left: {charsLeft[1]}{" "}
-            </span>
-            <span className="label-text-alt">Re-generate</span>
-          </label>
-        </div>
-
-        {/* Description */}
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-semibold">Description</span>
-            {/* <span className="label-text-alt">Alt label</span> */}
-          </label>
-          <textarea
-            value={textAreaValue}
-            onChange={(e) => setTextAreaValue(e.target.value)}
-            maxLength={260}
-            className="textarea-bordered textarea h-24 w-full rounded-sm shadow-md"
-            placeholder="Description"
-          ></textarea>
-          <label className="label">
-            <span className="label-text-alt">
-              Characters left: {charsLeft[2]}{" "}
-            </span>
-            <span className="label-text-alt">Re-generate</span>
-          </label>
-        </div>
+         </div>
       </div>
     </>
   );
