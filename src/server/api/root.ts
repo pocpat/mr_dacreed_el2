fe122ab31eb0c28929b87442cf925f41fddfee4a
Elena Kroupkin <pocoat@gmail.com>
2023-08-09 11:25:55 +1200

    delete page JonoPage2

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,60 +1,9 @@
-import { libraryRouter } from "~/server/api/routers/library";
-import { createTRPCRouter } from "~/server/api/trpc";
-import { topicRouter } from "./routers/topic";
-
-import {PostapiRouter} from "./routers/POSTAPI";
-
-
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
-export const appRouter = createTRPCRouter({
-  library: libraryRouter,
-  topic: topicRouter,
-  POSTAPI: PostapiRouter
-});
-
-
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
-
-
-// import { libraryRouter } from "~/server/api/routers/library";
-// import { createTRPCRouter } from "~/server/api/trpc";
-// import { exampleRouter } from "./routers/example";
-// import { topicRouter } from "./routers/topic";
-// import { noteRouter } from "./routers/note";
-
-// /**
-//  * This is the primary router for your server.
-//  *
-//  * All routers added in /api/routers should be manually added here.
-//  */
-// export const appRouter: AppRouter = createTRPCRouter({
-//   library: libraryRouter,
-//   example: exampleRouter,
-//   topic: topicRouter,
-//   note: noteRouter,
-// })
-//   .merge("topic.", topicRouter)
-//   .createContext(({ req }) => {
-//     return {
-//       session: req.session,
-//     };
-//   });
-
-// // export type definition of API
-// export type AppRouter = typeof appRouter;
-
-
 
 import { libraryRouter } from "~/server/api/routers/library";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { topicRouter } from "./routers/topic";
 import {PostapiRouter} from "./routers/POSTAPI";
+import { testStringRouter } from "./routers/testString";
 
 
 /**
@@ -64,7 +13,6 @@ import {PostapiRouter} from "./routers/POSTAPI";
  */
 export const appRouter = createTRPCRouter({
   testString: testStringRouter,
-  example: exampleRouter,
   library: libraryRouter,
   topic: topicRouter,
   POSTAPI: PostapiRouter
@@ -76,31 +24,5 @@ export const appRouter = createTRPCRouter({
 export type AppRouter = typeof appRouter;
 
 
-// import { libraryRouter } from "~/server/api/routers/library";
-// import { createTRPCRouter } from "~/server/api/trpc";
-// import { exampleRouter } from "./routers/example";
-// import { topicRouter } from "./routers/topic";
-// import { noteRouter } from "./routers/note";
-
-// /**
-//  * This is the primary router for your server.
-//  *
-//  * All routers added in /api/routers should be manually added here.
-//  */
-// export const appRouter: AppRouter = createTRPCRouter({
-//   library: libraryRouter,
-//   example: exampleRouter,
-//   topic: topicRouter,
-//   note: noteRouter,
-// })
-//   .merge("topic.", topicRouter)
-//   .createContext(({ req }) => {
-//     return {
-//       session: req.session,
-//     };
-//   });
-
-// // export type definition of API
-// export type AppRouter = typeof appRouter;
 
 
