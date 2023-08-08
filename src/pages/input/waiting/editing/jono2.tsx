bd133773cd6c46e072db8e50ed99a5d4a50e09f0
Jono Padoa <122724444+JonoPadoa@users.noreply.github.com>
2023-08-09 11:42:48 +1200

    Sorted out API errors.  Moved DB POST and GET into Jono2.  All working.  Also had to re-add in the new Schema model - CourseQuestion.  Should be working now.

--- src/pages/input/waiting/editing/jono2.tsx
+++ src/pages/input/waiting/editing/jono2.tsx
@@ -29,7 +29,7 @@ const Jono2 = () => {
             </div>
 
             <div>
-              <Content />
+              <TestStringInput />
             </div>
             <div>
               <JPButtonRender />
@@ -46,30 +46,31 @@ const Jono2 = () => {
 
 export default Jono2;
 
-type Topic = {
+type TestString = {
   id: string;
-  createdAt: Date;
-  updatedAt: Date;
-  title: string;
+  // createdAt: Date;
+  // updatedAt: Date;
+  testInput: string;
   userId: string;
 };
 
-const Content: React.FC = () => {
-  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
-  const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
-    undefined, // no input
-    {
-      onSuccess: (data) => {
-        if (data && data.length > 0 && data[0]) {
-          setSelectedTopic(data[0]);
-        } else {
-          setSelectedTopic(null);
-        }
-      },
-    }
-  );
+const TestStringInput: React.FC = () => {
+  const [selectedInput, setSelectedInput] = useState<TestString | null>(null);
+  const { data: testStrings, refetch: refetchTopics } =
+    api.testString.getAll.useQuery(
+      undefined, // no input
+      {
+        onSuccess: (data) => {
+          if (data && data.length > 0 && data[0]) {
+            setSelectedInput(data[0]);
+          } else {
+            setSelectedInput(null);
+          }
+        },
+      }
+    );
 
-  const createTopic = api.topic.create.useMutation({
+  const createTestString = api.testString.create.useMutation({
     onSuccess: () => {
       console.log("We think its going to db");
       void refetchTopics();
@@ -78,44 +79,43 @@ const Content: React.FC = () => {
 
   return (
     <div className="mb-4 mt-4 flex flex-col items-start justify-center rounded-lg border-slate-100 bg-white p-4">
-      <div className="col-span-3">
-        {selectedTopic && (
-          <div className="m-2 rounded-lg border-2 p-4">
-            Selected topic: {selectedTopic.title}
-          </div>
-        )}
+      <div className="col-span-3"></div>
+      <div className="divider">
+        <input
+          type="text"
+          placeholder="New Test String"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTestString.mutate({
+                testInput: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
       </div>
-      <div className="col-span-3">
+      <h1>Posted data:</h1>
+      <div>
         <ul className="menu m-2 rounded-lg border-2 p-4">
-          {topics?.map((topic) => (
-            <li key={topic.id}>
+          {testStrings?.map((testString) => (
+            <li key={testString.id}>
               <a
                 href="#"
                 onClick={(evt) => {
                   evt.preventDefault();
-                  setSelectedTopic(topic);
+                  // setSelectedInput();
                 }}
               >
-                {topic.title}
+                {testString.testInput}
               </a>
             </li>
           ))}
         </ul>
+        <button className="ml-4 rounded-lg bg-[#064e3b] p-2 text-white hover:bg-[#059669]">
+          Save Changes
+        </button>
       </div>
-      <div className="divider"></div>
-      <input
-        type="text"
-        placeholder="New Topic"
-        className="input-bordered input input-sm w-full"
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            createTopic.mutate({
-              title: e.currentTarget.value,
-            });
-            e.currentTarget.value = "";
-          }
-        }}
-      />
     </div>
   );
 };
