50683e0b7616ed176ba83ee6fe71a40d36dde894
Jono Padoa <122724444+JonoPadoa@users.noreply.github.com>
2023-08-10 12:39:12 +1200

    Cleaned up API POSt and GET to display properly in individula fields on the header page of the accordion.  Next steps working on updating the db.

--- src/pages/components/elenasComponents/HeaderBody.tsx
+++ src/pages/components/elenasComponents/HeaderBody.tsx
@@ -4,63 +4,11 @@ import { api } from "~/utils/api";
 const HeaderBody = () => {
   return (
     <>
-      <CourseHeaderInput />
-      {/* <TestInput /> */}
       <div
-        className="border-box flex flex-col items-center justify-start bg-gray-400 p-4 "
+        className="border-box flex flex-col bg-gray-400 p-4 "
         style={{ width: "100%", height: "100vh" }}
       >
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-semibold">Title</span>
-          </label>
-
-          <textarea
-            placeholder="Title here"
-            className="shadow-inner-md textarea-bordered textarea textarea-xs w-full rounded-sm"
-            style={{ width: "100%" }}
-          ></textarea>
-        </div>
-
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-semibold">Sub-heading</span>
-          </label>
-
-          <textarea
-            placeholder="Sub-heading here"
-            className="textarea-bordered textarea textarea-xs w-full rounded-sm shadow-md"
-            style={{ width: "100%" }}
-          ></textarea>
-        </div>
-
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-semibold">Description</span>
-          </label>
-
-          <textarea
-            placeholder="Description here"
-            className="textarea-bordered textarea  h-48 w-full rounded-sm shadow-md"
-            style={{ width: "100%" }}
-          ></textarea>
-        </div>
-        <div></div>
-        <textarea
-          id="title"
-          className="mt-8 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          placeholder="Title will be populated here:"
-        ></textarea>
-        <textarea
-          id="sub-heading"
-          className="mt-8 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          placeholder="Sub-heading will be populated here:"
-        ></textarea>
-        <textarea
-          id="description"
-          className="mt-8 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          placeholder="Description will be populated here:"
-        ></textarea>
+        <CourseHeaderInput />
       </div>
     </>
   );
@@ -96,12 +44,13 @@ const CourseHeaderInput: React.FC = () => {
   });
 
   return (
-    <div className="mb-4 mt-4 flex flex-col items-start justify-center rounded-lg border-slate-100 bg-white p-4">
-      <div className="col-span-3">
+    <div className="mb-4 mt-4 flex w-full flex-col items-start justify-items-start rounded border-slate-100 bg-white p-4">
+      <div className="m-2 flex w-full flex-col p-2">
+        {/* INPUT FOR TITLE */}
         <input
           type="text"
-          placeholder="Title goes here:"
-          className="input-bordered input input-sm w-full"
+          placeholder="Title goes here"
+          className="input-bordered input input-sm m-2 h-12 w-auto"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               createCourseHeader.mutate({
@@ -113,10 +62,11 @@ const CourseHeaderInput: React.FC = () => {
             }
           }}
         />
+        {/* INPUT FOR SUB HEADING */}
         <input
           type="text"
           placeholder="Sub-heading goes here:"
-          className="input-bordered input input-sm w-full"
+          className="input-bordered input input-sm m-2 h-12 w-auto"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               createCourseHeader.mutate({
@@ -128,10 +78,11 @@ const CourseHeaderInput: React.FC = () => {
             }
           }}
         />
+        {/* INPUT FOR DESCRIPTION */}
         <input
           type="text"
           placeholder="Description goes here:"
-          className="input-bordered input input-sm w-full"
+          className="input-bordered input input-sm m-2 h-12 w-auto"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               createCourseHeader.mutate({
@@ -144,27 +95,34 @@ const CourseHeaderInput: React.FC = () => {
           }}
         />
       </div>
-
-      <h1>Posted data:</h1>
       <div>
-        <ul className="menu m-2 rounded-lg border-2 p-4">
-          {courseHeaders?.map((courseHeader) => (
-            <li key={courseHeader.id}>
-              <a
-                href="#"
-                onClick={(evt) => {
-                  evt.preventDefault();
-                  // setSelectedInput();
-                }}
-              >
-                {courseHeader.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <button className="ml-4 rounded-lg bg-[#064e3b] p-2 text-white hover:bg-[#059669]">
-          Save Changes
-        </button>
+        <div className="ml-6">
+          <div className="bg-slate-400 p-2">
+            {courseHeaders?.map((courseHeader) => {
+              if (courseHeader.title) {
+                return <div key={courseHeader.id}>{courseHeader.title}</div>;
+              }
+            })}
+          </div>
+          <div className="border-solid border-black bg-slate-300 p-2">
+            {courseHeaders?.map((courseHeader) => {
+              if (courseHeader.subHeading) {
+                return (
+                  <div key={courseHeader.id}>{courseHeader.subHeading}</div>
+                );
+              }
+            })}
+          </div>
+          <div className="border-solid border-black bg-slate-200 p-2">
+            {courseHeaders?.map((courseHeader) => {
+              if (courseHeader.description) {
+                return (
+                  <div key={courseHeader.id}>{courseHeader.description}</div>
+                );
+              }
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
