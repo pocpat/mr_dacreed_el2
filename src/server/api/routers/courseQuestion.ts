4e83775b5db97f803feec439fb1042e9abf3a576
Elena Kroupkin <pocoat@gmail.com>
2023-08-15 20:49:02 +1200

    save before adding <labels> into Question1

--- src/server/api/routers/courseQuestion.ts
+++ src/server/api/routers/courseQuestion.ts
@@ -1,5 +1,5 @@
+// src/server/api/routers/courseQuestion.ts
 import { z } from "zod";
-
 import {
   createTRPCRouter,
   protectedProcedure,
@@ -10,17 +10,13 @@ export const courseQuestionRouter = createTRPCRouter({
   getCourses: publicProcedure.query(({ ctx }) => {
     if (ctx.auth?.userId) {
       return ctx.prisma.courseQuestion.findMany({
-        where: {
-          userId: ctx.auth.userId,
-        },
+        where: { userId: ctx.auth.userId },
       });
     } else {
       console.log("User not authenticated. => ctx.auth.userId is null <=");
     }
   }),
-
   create: protectedProcedure
-
     .input(
       z.object({
         question: z.string(),
@@ -32,17 +28,16 @@ export const courseQuestionRouter = createTRPCRouter({
         answer6: z.string().optional(),
       })
     )
-
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.courseQuestion.create({
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.courseQuestion.create({
         data: {
           question: input.question,
           answer1: input.answer1,
           answer2: input.answer2,
-          answer3: input.answer3,
-          answer4: input.answer4,
-          answer5: input.answer5,
-          answer6: input.answer6,
+          answer3: input.answer3  ?? " ",
+          answer4: input.answer4  ?? " ",
+          answer5: input.answer5  ?? " ",
+          answer6: input.answer6  ?? " ",
           userId: ctx.auth.userId,
         },
       });
