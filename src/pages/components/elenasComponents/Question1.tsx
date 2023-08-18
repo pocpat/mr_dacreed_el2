33ab0a861ebe3026856e27294a71d5c29765b7bd
Elena Kroupkin <pocoat@gmail.com>
2023-08-18 23:21:22 +1200

    design Add menu

--- src/pages/components/elenasComponents/Question1.tsx
+++ src/pages/components/elenasComponents/Question1.tsx
@@ -172,29 +172,78 @@ const [guidance, setGuidance] = useState("");
           value="Create Q&A"
         ></button>
       </div>
-      <div className="w-82 dropdown dropdown-top flex content-center justify-center rounded-sm">
-        <label tabIndex={0} className="btn m-1 rounded-sm">
-          ADD
+      <div className="w-82 dropdown dropdown-top flex content-center justify-center rounded-sm ">
+        <label tabIndex={0} className=" m-1 flex flex-row items-center ">
+         <p className="mr-2">Add </p>   
+          <svg width="14" height="14" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.4 5.4V3H6.6V5.4H9V6.6H6.6V9H5.4V6.6H3V5.4H5.4ZM6 12C2.68629 12 0 9.31368 0 6C0 2.68629 2.68629 0 6 0C9.31368 0 12 2.68629 12 6C12 9.31368 9.31368 12 6 12ZM6 10.8C8.65098 10.8 10.8 8.65098 10.8 6C10.8 3.34903 8.65098 1.2 6 1.2C3.34903 1.2 1.2 3.34903 1.2 6C1.2 8.65098 3.34903 10.8 6 10.8Z" fill="#1E425C"/>
+</svg>
+
         </label>
         <ul
           tabIndex={0}
           className="menu dropdown-content z-[1] bg-base-100 p-2 shadow  "
         >
+
+
           <li>
-            <a onClick={addNewAnswer}> Answers Add more answer boxes </a>
+       <section className="flex flex-row" onClick={addNewAnswer}>
+        <img src="/icons/answers.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
+        <div>
+            <a  className="font-bold"> Answers  </a>
+            <p>Add more answer boxes</p>
+            </div>
+            </section>
           </li>
+
+
+
           <li>
-            <a onClick={() => setIsModal4Open(true)}>
-              Media Upload or embed with a link
-            </a>{" "}
-            {/* Open modal on media click */}{" "}
+          <section className="flex flex-row"  onClick={() => setIsModal4Open(true)}>
+          <img src="/icons/media.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
+          <div>
+            <a 
+            className="font-bold">
+              Media 
+            </a>
+            <p>Upload or embed with a link</p>
+            </div>
+            </section>
           </li>
+
+
+
           <li>
-            <a>Commentary Add commentary for course</a>
+            {/* onClick => to add Commentary component */}
+          <section className="flex flex-row"  onClick={() => setIsModal4Open(true)}>  
+          <img src="/icons/commentary.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
+          <div>
+            <a 
+            className="font-bold">
+              Commentary 
+            </a>
+            <p>Add commentary for course</p>
+            </div>
+            </section>
           </li>
+
+
+
           <li>
-            <a>Guidance Add guidance for course</a>
+            {/* onClick => to add Guidance component */}
+          <section className="flex flex-row"  onClick={() => setIsModal4Open(true)}>  
+          <img src="/icons/guidance.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
+          <div>
+            <a 
+            className="font-bold">
+              Guidance 
+            </a>
+            <p>Add guidance for course</p>
+            </div>
+            </section>
           </li>
+
+       
         </ul>
       </div>
       {isModal4Open && (
