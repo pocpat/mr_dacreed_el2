82a8a7867546f734d5878e8c1fb0edd8e594ebce
Elena Kroupkin <pocoat@gmail.com>
2023-08-21 09:51:35 +1200

    uploaded img is centered with max-w

--- src/pages/components/elenasComponents/Question1.tsx
+++ src/pages/components/elenasComponents/Question1.tsx
@@ -36,8 +36,8 @@ const QAForm: React.FC = () => {
   const [answerValues, setAnswerValues] = useState<string[]>(["", ""]);
   const [isModal4Open, setIsModal4Open] = useState(false);
   const [commentary, setCommentary] = useState("");
-const [guidance, setGuidance] = useState("");
-const [ uploadedImgs, setUploadedImgs ] = useState<string | null>(null);
+  const [guidance, setGuidance] = useState("");
+  const [uploadedImgs, setUploadedImgs] = useState<string | null>(null);
 
   const { mutate: createQuestion } = api.courseQuestion.create.useMutation({});
   // const { mutate: createCommentary } = api.courseQuestion.create.useMutation({});
@@ -101,10 +101,9 @@ const [ uploadedImgs, setUploadedImgs ] = useState<string | null>(null);
   const handleMediaUpload = (media: string) => {
     setUploadedImgs(media);
     setIsModal4Open(false);
-
   };
   return (
-    <form onSubmit={handleSubmit} className="m-0 bg-lightsecondaryd:">
+    <form onSubmit={handleSubmit} className="bg-lightsecondaryd: m-0">
       <span className="ml-3  font-bold text-accentd">Question 1</span>
       <div>
         {/* question */}
@@ -156,7 +155,7 @@ const [ uploadedImgs, setUploadedImgs ] = useState<string | null>(null);
               </div>
             ))}
           </div>
-          
+
           <Commentary
             placeholder=""
             type="text"
@@ -165,13 +164,12 @@ const [ uploadedImgs, setUploadedImgs ] = useState<string | null>(null);
             charsLeft={150 - commentary.length}
           />
 
-          <Guidance 
-          placeholder="" 
-          type="" 
-          onChange={(e) => setGuidance(e.target.value)}
-          value={guidance}
-          charsLeft={150 - guidance.length}
-          
+          <Guidance
+            placeholder=""
+            type=""
+            onChange={(e) => setGuidance(e.target.value)}
+            value={guidance}
+            charsLeft={150 - guidance.length}
           />
         </div>
         <button
@@ -180,95 +178,108 @@ const [ uploadedImgs, setUploadedImgs ] = useState<string | null>(null);
           value="Create Q&A"
         ></button>
       </div>
-{/* add uploaded image here  */}
-      <div className="flex flex-row justify-between">
-      {uploadedImgs && <img src={uploadedImgs} alt="Uploaded" />}
-</div>
-
-
-
+      {/* add uploaded image here  */}
+      <div className="flex  items-center justify-center ">
+        {uploadedImgs && (
+          <div className="max-w-screen-2xl px-[20%]">
+            <img src={uploadedImgs} alt="Uploaded" />
+          </div>
+        )}
+      </div>
 
       <div className="w-82 dropdown dropdown-top flex content-center justify-center rounded-sm ">
         <label tabIndex={0} className=" m-1 flex flex-row items-center ">
-         <p className="mr-2">Add </p>   
-          <svg width="14" height="14" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.4 5.4V3H6.6V5.4H9V6.6H6.6V9H5.4V6.6H3V5.4H5.4ZM6 12C2.68629 12 0 9.31368 0 6C0 2.68629 2.68629 0 6 0C9.31368 0 12 2.68629 12 6C12 9.31368 9.31368 12 6 12ZM6 10.8C8.65098 10.8 10.8 8.65098 10.8 6C10.8 3.34903 8.65098 1.2 6 1.2C3.34903 1.2 1.2 3.34903 1.2 6C1.2 8.65098 3.34903 10.8 6 10.8Z" fill="#1E425C"/>
-</svg>
-
+          <p className="mr-2">Add </p>
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5.4 5.4V3H6.6V5.4H9V6.6H6.6V9H5.4V6.6H3V5.4H5.4ZM6 12C2.68629 12 0 9.31368 0 6C0 2.68629 2.68629 0 6 0C9.31368 0 12 2.68629 12 6C12 9.31368 9.31368 12 6 12ZM6 10.8C8.65098 10.8 10.8 8.65098 10.8 6C10.8 3.34903 8.65098 1.2 6 1.2C3.34903 1.2 1.2 3.34903 1.2 6C1.2 8.65098 3.34903 10.8 6 10.8Z"
+              fill="#1E425C"
+            />
+          </svg>
         </label>
         <ul
           tabIndex={0}
           className="menu dropdown-content z-[1] bg-base-100 p-2 shadow  "
         >
-
-
           <li>
-       <section className="flex flex-row" onClick={addNewAnswer}>
-        <img src="/icons/answers.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
-        <div>
-            <a  className="font-bold"> Answers  </a>
-            <p>Add more answer boxes</p>
-            </div>
+            <section className="flex flex-row" onClick={addNewAnswer}>
+              <img
+                src="/icons/answers.png"
+                alt="answer"
+                className="mr-2 inline-block h-8 w-8"
+              />
+              <div>
+                <a className="font-bold"> Answers </a>
+                <p>Add more answer boxes</p>
+              </div>
             </section>
           </li>
 
-
-
           <li>
-          <section className="flex flex-row"  onClick={() => setIsModal4Open(true)}>
-          <img src="/icons/media.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
-          <div>
-            <a 
-            className="font-bold">
-              Media 
-            </a>
-            <p>Upload or embed with a link</p>
-            </div>
+            <section
+              className="flex flex-row"
+              onClick={() => setIsModal4Open(true)}
+            >
+              <img
+                src="/icons/media.png"
+                alt="answer"
+                className="mr-2 inline-block h-8 w-8"
+              />
+              <div>
+                <a className="font-bold">Media</a>
+                <p>Upload or embed with a link</p>
+              </div>
             </section>
           </li>
-
-
 
           <li>
             {/* onClick => to add Commentary component */}
-          <section className="flex flex-row"  onClick={() => setIsModal4Open(true)}>  
-          <img src="/icons/commentary.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
-          <div>
-            <a 
-            className="font-bold">
-              Commentary 
-            </a>
-            <p>Add commentary for course</p>
-            </div>
+            <section
+              className="flex flex-row"
+              onClick={() => setIsModal4Open(true)}
+            >
+              <img
+                src="/icons/commentary.png"
+                alt="answer"
+                className="mr-2 inline-block h-8 w-8"
+              />
+              <div>
+                <a className="font-bold">Commentary</a>
+                <p>Add commentary for course</p>
+              </div>
             </section>
           </li>
-
-
 
           <li>
             {/* onClick => to add Guidance component */}
-          <section className="flex flex-row"  onClick={() => setIsModal4Open(true)}>  
-          <img src="/icons/guidance.png" alt="answer" className="inline-block h-8 w-8 mr-2" />
-          <div>
-            <a 
-            className="font-bold">
-              Guidance 
-            </a>
-            <p>Add guidance for course</p>
-            </div>
+            <section
+              className="flex flex-row"
+              onClick={() => setIsModal4Open(true)}
+            >
+              <img
+                src="/icons/guidance.png"
+                alt="answer"
+                className="mr-2 inline-block h-8 w-8"
+              />
+              <div>
+                <a className="font-bold">Guidance</a>
+                <p>Add guidance for course</p>
+              </div>
             </section>
           </li>
-
-       
         </ul>
       </div>
       {isModal4Open && (
         <Modal4 onClose={() => setIsModal4Open(false)} isVisible={true}>
-           <UploadImgs onMediaUpload={handleMediaUpload} />
+          <UploadImgs onMediaUpload={handleMediaUpload} />
         </Modal4>
       )}
-  
-
     </form>
   );
 };
